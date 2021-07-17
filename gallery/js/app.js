8f25e7b4100d0f415d06515b6064e5fdd53bc62e
guosonghao <guosonghao@yunjinhz.com>
2021-07-17 08:18:20 +0800

    gallery

---
+++ gallery/js/app.js
@@ -0,0 +1,61 @@
+﻿var title = "Title";
+; window.QueryString = function (name) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(window.location.href);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return window.decodeURIComponent(results[2].replace(/\+/g, ' '));
+};
+
+window.LocalStorage = function (key, value) {
+    if (!key) {
+        return null;
+    }
+    if (value) {
+        localStorage.setItem(key, value);
+        return value;
+    }
+    return localStorage.getItem(key);
+};
+
+window.Global = new Vue({
+    el: '#app',
+    data: {
+        Title: title,
+        PageIndex: 0,
+        TotalPages: 0,
+        Data: [""]
+    },
+    methods: {}
+});
+
+window.GetDateString = function () {
+    return new XDate().toString("yyyyMMddHHmmssfff");
+};
+
+window.GotoPage = function (page) {
+    window.LocalStorage("page", page);
+    $.get("/file/list/" + page, { date: window.GetDateString() }, function (data) {
+        data.Title = title;
+        Global.$data = data;
+    });
+};
+
+window.DeleteFile = function (url) {
+    $.post("/file/delete", { u: url }, function (data) {
+        console.log(data);
+        window.location.reload();
+    });
+};
+
+$(document).ready(function () {
+    var page = window.QueryString("page");
+    if (!page) {
+        page = window.LocalStorage("page");
+        if (!page) {
+            page = 1;
+        }
+    }
+    window.GotoPage(page);
+});
---
+++ study/js/app.js
@@ -0,0 +1,88 @@
+var viewModel = {
+    title: "开源项目",
+    projectName: "asp.net study",
+    tagline: "开发中常用的一些开源项目",
+    author: "郭松浩",
+    packages:
+    [
+        {
+            title: "数据持久",
+            name: "数据持久/ORM",
+            items: [
+                { name: "Dapper.NET", description: "A simple object mapper for .Net", github: "StackExchange/dapper-dot-net", nuget: "Dapper" },
+                { name: "PetaPoco", description: "A tiny ORM-ish thing for your POCO's", github: "CollaboratingPlatypus/PetaPoco", nuget: "PetaPoco" },
+                { name: "OrmLite", description: "Fast, Simple, Typed ORM for .NET", github: "ServiceStack/ServiceStack.OrmLite", nuget: "ServiceStack.OrmLite" },
+                { name: "Xinchen.XLinq", description: "一个轻量级ORM框架,使用方法和EF一样", github: "", nuget: "Xinchen.XLinq" }
+            ]
+        },
+        {
+            title: "依赖注入",
+            name: "依赖注入/IOC",
+            items: [
+                { name: "Autofac", description: "An addictive .NET IoC container", github: "autofac/Autofac", nuget: "Autofac" },
+                { name: "Funq", description: "A fast DI container you can understand", github: "w0rd-driven/Funq", nuget: "Funq" }
+            ]
+        },
+        {
+            title: "面向切面",
+            name: "面向切面/AOP",
+            items: [
+                { name: "LinFu.DynamicProxy", description: "A small, lightweight dynamic proxy library for the .NET framework", github: "philiplaureano/LinFu.DynamicProxy", nuget: "LinFu.DynamicProxy.OfficialRelease" },
+                { name: "SimpleAspects", description: "A simple AOP framework", github: "hcesar/SimpleAspects", nuget: "SimpleAspects" }
+            ]
+        },
+        {
+            title: "对象映射",
+            name: "对象映射/ObjectMapper",
+            items: [
+                { name: "AutoMapper", description: "A convention-based object-object mapper in .NET", github: "AutoMapper/AutoMapper", nuget: "AutoMapper" },
+                { name: "NLite", description: "NLite 框架旨在提供一个轻量级的组件库-DI，Aop,MessageBus, OO Mapper，国际化,数据校验，日志，异常管理，多线程等", github: "netcasewqs/NLite", nuget: "NLite" },
+                { name: "TinyMapper", description: "A quick object-object mapper for .NET", github: "TinyMapper/TinyMapper", nuget: "TinyMapper" }
+            ]
+        },
+        {
+            title: "网络服务",
+            name: "网络服务/Network",
+            items: [
+                { name: "Flux", description: "Lightweight web server for .NET and Mono", github: "markrendle/Flux", nuget: "Flux" },
+                { name: "Nancy", description: "Lightweight, low-ceremony, framework for building HTTP based services on .Net and Mono", github: "NancyFx/Nancy", nuget: "Nancy" },
+                { name: "SelfishHttp", description: "Self hosting HTTP server for testing in .net", github: "featurist/SelfishHttp", nuget: "SelfishHttp" },
+                { name: "FastHttpRequest", description: "HTTP Helpers for C# / .NET", github: "vla/FastHttpRequest", nuget: "FastHttpRequest" },
+                { name: "TinyHTTP-PCL", description: "A tiny HTTP abstraction layer for PCL", github: "Deathspike/TinyHTTP-PCL", nuget: "TinyHTTP-PCL" }
+            ]
+        },
+        {
+            title: "日志组件",
+            name: "日志组件/Logging",
+            items: [
+                { name: "log4net", description: "The Apache log4net library is a tool to help the programmer output log statements to a variety of output targets", github: "apache/log4net", nuget: "log4net" },
+                { name: "NLog", description: "Advanced .NET, Silverlight and Xamarin Logging", github: "NLog/NLog", nuget: "NLog" }
+            ]
+        },
+		{
+            title: "序列化",
+            name: "序列化/Serialize",
+            items: [
+				{ name: "Json.NET", description: "Json.NET is a popular high-performance JSON framework for .NET", github: "JamesNK/Newtonsoft.Json", nuget: "Newtonsoft.Json" },
+                { name: "SharpSerializer", description: "SharpSerializer ports to MonoForAndroid, MonoTouch, WindowsPhone", github: "moljac/MonoVersalSharpSerializer", nuget: "SharpSerializer" }
+            ]
+        },
+        {
+            title: "其它项目",
+            name: "其它项目/Others",
+            items: [
+                { name: "EPPlus", description: "EPPlus is a .net library that reads and writes Excel 2007/2010 files using the Open Office Xml format (xlsx)", github: "wenhx/EPPlus", nuget: "EPPlus" },
+                { name: "FluentValidation", description: "A small validation library for .NET that uses a fluent interface and lambda expressions for building validation rules", github: "JeremySkinner/FluentValidation", nuget: "FluentValidation" },
+                { name: "FluentXml", description: ".NET XML library providing fluent XML for querying and creating XML documents", github: "remi/FluentXml", nuget: "FluentXml" },
+                { name: "HtmlAgilityPack", description: "This is an agile HTML parser that builds a read/write DOM and supports plain XPATH or XSLT", github: "floytinax/HtmlAgilityPack", nuget: "HtmlAgilityPack" },                
+                { name: "LiteDB", description: "A simple embedded .NET NoSQL Document Store in a single data file", github: "mbdavid/LiteDB", nuget: "LiteDB" },
+                { name: "NMemory", description: "NMemory is a lightweight non-persistent in-memory relational database engine that is purely written in C# and can be hosted by .NET applications", github: "tamasflamich/nmemory", nuget: "NMemory" },
+                { name: "Quartz.NET", description: "Quartz Enterprise Scheduler .NET", github: "quartznet/quartznet", nuget: "Quartz" }
+            ]
+        }
+    ]
+};
+var vm = new Vue({
+    el: '#app',
+    data: viewModel
+});
